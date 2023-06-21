9964e774c1105f9b79bc6b9190caab3ee6a9c31a
VarBoff <vinirechboff@gmail.com>
2023-06-20 21:57:48 -0300

    adicionando servicos e ligando eles ao namingServer

---
+++ servicePlanosDeVoo/src/main/java/com/facul/T2_ProjArq/ServicePlanosDeVooApplication.java
@@ -0,0 +1,13 @@
+package com.facul.T2_ProjArq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePlanosDeVooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePlanosDeVooApplication.class, args);
+	}
+
+}
---
+++ serviceRotas/src/test/java/com/facul/T2_ProjArq/ServiceCadastroDeRotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facul.T2_ProjArq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCadastroDeRotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
